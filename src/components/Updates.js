25b04463eba60d365c3d7f4485710a850aedb7a3
Rodney <rwaltonj@eecs.wsu.edu>
2020-05-21 07:04:20 -0700

    added outline and border around updates for better readability

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -9,8 +9,8 @@ class Updates extends React.Component {
 
     render() {
         return (
-            <div className="container privacy-page-container">
-                <div className="privacy-page row justify-content-md-center">
+            <div className="container update-page-container">
+                <div className="update-page row justify-content-md-center">
                     <div className="col-12 pb-5 d-flex justify-content-center">
                         <h1>Updates</h1>
                     </div>
@@ -21,6 +21,13 @@ class Updates extends React.Component {
                         <h5>
                             Version 1.1: Fixed buttons to scale better and balance changes to the difficulty, circles now grow slower in all settings.
                             Also some other small UI changes made as well. Game mode now has border to better show game board.
+                        </h5>
+                    </div>
+                    <div className="col-12 pb-5 d-flex justify-content-start">
+                        <h5>
+                            Version 1.2: Fixed buttons again to scale better. Homepage now has moving background. Changed custom mode so each mode now
+                            only displays inputs that will change the respective mode. For example, precision no longer has min/max radius or radius
+                            change input fields
                         </h5>
                     </div>
                 </div>
